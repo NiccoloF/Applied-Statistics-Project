67007fb6f5e5f744e4e271f89a4e4dc45df43d1d
Matteo-Coppellotti <matteo.coppellotti@mail.polimi.it>
2023-03-20 09:55:00 +0100

    changed read path

--- Forecasting_Day_Ahead_Elettricity_prices.R
+++ Forecasting_Day_Ahead_Elettricity_prices.R
@@ -3,7 +3,7 @@
 ################################################################################
 
 # Import data_set 
-Elettricity_price <- read.csv("/Users/niccoloferesini/Desktop/dam-ahead/ml_data.csv", header = TRUE)
+Elettricity_price <- read.csv("./Data_set/ml_data.csv", header = TRUE)
 head(Elettricity_price)
 dim(Elettricity_price)
 str(Elettricity_price)
