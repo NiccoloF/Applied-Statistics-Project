f201afdf7c3919df9af90ab4d0c735fd0d960d52
Matteo-Coppellotti <matteo.coppellotti@mail.polimi.it>
2023-03-21 09:57:59 +0100

    added heatmap and fixed import

--- Forecasting_Day_Ahead_Elettricity_prices.R
+++ Forecasting_Day_Ahead_Elettricity_prices.R
@@ -3,8 +3,18 @@
 ################################################################################
 
 # Import data_set 
-Electricity_price <- read.csv("./Data_set/ml_data.csv", header = TRUE)
+Electricity_price <- read.csv("./Data_set/ml_data.csv", header = TRUE, 
+                              row.names = 1)
 head(Electricity_price)
 dim(Electricity_price)
 str(Electricity_price)
 summary(Electricity_price)
+
+
+# Correlation matrix and heat map visualization
+
+corr <- cor(Electricity_price)
+x11() #IF WORKING ON MAC BASAED SYSTEMS CHANGE ALL x11 OCCURRENCIES WITH QUARTZ
+heatmap(corr)
+
+graphics.off()
