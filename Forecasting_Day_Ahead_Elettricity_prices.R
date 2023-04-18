e9b37eb6121d8061225812c86a66ebdf57703fe1
LucaCaroselli <luca.caroselli@mail.polimi.it>
2023-04-18 15:34:02 +0200

    vuoccacao

--- Forecasting_Day_Ahead_Elettricity_prices.R
+++ Forecasting_Day_Ahead_Elettricity_prices.R
@@ -24,14 +24,19 @@ heatmap(corr)
 
 # compute the mean by day to see if there is a seasonal trend (2018)
 library(lubridate)
+library(ggplot2)
 Electricity_price_2018 <- Electricity_price[row.names(Electricity_price) < "2019-01-01 00:00:00",]
-price_daily_2018 <- aggregate(Electricity_price_2018$dam, by = list(Electricity_price_2018$day,Electricity_price_2018$month), FUN = mean)
-plot(price_daily_2018[,3], type="l")
+price_daily_2018 <- aggregate(Electricity_price_2018$Price, by=list(Electricity_price_2018$day,Electricity_price_2018$month), FUN = mean)
+plot(price_daily_2018[,3], type="l", ylab = "daily average price", xlab= "days")
+#ggplot( data = price_daily_2018, mapping = aes( y = x) )
+
 
 # compute the mean by day to see if there is a seasonal trend (2019)
 Electricity_price_2019 <- Electricity_price[row.names(Electricity_price) > "2018-12-31 23:00:00",]
-price_daily_2019 <- aggregate(Electricity_price_2019$dam, by = list(Electricity_price_2019$day,Electricity_price_2019$month), FUN = mean)
+price_daily_2019 <- aggregate(Electricity_price_2019$Price, by = list(Electricity_price_2019$day,Electricity_price_2019$month), FUN = mean)
 lines(price_daily_2019[,3], type="l",col="red")
+legend("2018", "2019")
+
 
 # compute the hourly prices 
 price_hour_2018<- aggregate(Electricity_price_2018$dam, by = list(Electricity_price_2018$hour), FUN = mean)
@@ -41,3 +46,18 @@ lines(price_hour_2019, type="l", col="red")
 
 plot(Electricity_price$hour, Electricity_price$dam)
 lines(lines(price_hour_2018, type="l", col="blue"))
+
+
+#Compute the mean by month to see a seasonal trend for 2018 and 2019
+price_monthly_2018 <- aggregate(Electricity_price_2018$Price, by=list(Electricity_price_2018$month), FUN = mean)
+price_monthly_2019 <- aggregate(Electricity_price_2019$Price, by=list(Electricity_price_2019$month), FUN = mean)
+plot(price_monthly_2018, type="l",main="Monthly average price", ylab = "Price", xlab= "month", ylim=c(40, 80),lwd=4, col="blue")
+lines(price_monthly_2019, col= "red" ,lwd=5)
+legend("topleft", legend = c("2018","2019"), col = c("blue","red"), lty=1)
+
+# Compute the hourly mean 
+price_hourly_2018 <- aggregate(Electricity_price_2018$Price, by=list(Electricity_price_2018$hour), FUN = mean)
+price_hourly_2019 <- aggregate(Electricity_price_2019$Price, by=list(Electricity_price_2019$hour), FUN = mean)
+plot(price_hourly_2018, type="l",main="Hourly average price", ylab = "Price", xlab= "Day", ylim=c(40, 80),lwd=4, col="blue")
+lines(price_hourly_2019, col= "red" ,lwd=5)
+legend("topleft", legend = c("2018","2019"), col = c("blue","red"), lty=1, lwd=5)
