d42b5be6e16df0dc4eb1510eb8a741cf9a6eeea3
Matteo-Coppellotti <matteo.coppellotti@mail.polimi.it>
2023-04-18 10:45:13 +0200

    Changed plot of the means

--- Forecasting_Day_Ahead_Elettricity_prices.R
+++ Forecasting_Day_Ahead_Elettricity_prices.R
@@ -50,14 +50,6 @@ data_18 <- data[which((rownames(data) >= "2018-01-01 00:00:00" &
 data_19 <- data[which((rownames(data) >= "2019-01-01 00:00:00" &
     rownames(data) <= "2019-12-31 23:00:00")), ]
 
-# plot the data
-x11()
-par(mar = c(1, 1, 1, 1))
-par(mfcol = c(8, 4))
-for (i in seq_len(ncol(data_18_19))) {
-    plot(data_18_19[, i], type = "l", main = paste("Series", i))
-}
-
 # compute the monthly mean of dam in 2018
 dam_18_monthly <- aggregate(data_18$dam,
     by = list(month = data_18$month), FUN = mean)
@@ -67,19 +59,6 @@ dam_18_monthly <- dam_18_monthly$x - mean(data_18$dam)
 dam_19_monthly <- aggregate(data_19$dam,
     by = list(month = data_19$month), FUN = mean)
 dam_19_monthly <- dam_19_monthly$x - mean(data_19$dam)
-
-# plot the monthly mean of dam in 2018 and 2019 in the same graph
-x11()
-plot(dam_18_monthly, type = "l",
-    col = "blue",
-    main = "Monthly mean of dam in 2018 and 2019",
-    xlab = "Month", ylab = "Dam",
-    lwd = 5)
-lines(dam_19_monthly, col = "red", lwd = 5)
-legend("topleft", legend = c("2018", "2019"),
-    col = c("blue", "red"), lty = 1, cex = 0.8,
-    lwd = 5
-)
 
 # compute the daily mean of dam in 2018
 dam_18_weekday <- aggregate(data_18$dam,
@@ -91,19 +70,6 @@ dam_19_weekday <- aggregate(data_19$dam,
     by = list(day = data_19$weekday), FUN = mean)
 dam_19_weekday <- dam_19_weekday$x - mean(data_19$dam)
 
-# plot the daily mean of dam in 2018 and 2019 in the same graph
-x11()
-plot(dam_18_weekday, type = "l",
-    col = "blue",
-    main = "Dayly mean of dam in 2018 and 2019",
-    xlab = "Day", ylab = "Dam",
-    lwd = 5)
-lines(dam_19_weekday, col = "red", lwd = 5)
-legend("topleft", legend = c("2018", "2019"),
-    col = c("blue", "red"), lty = 1, cex = 0.8,
-    lwd = 5
-)
-
 # compute the hourly of dam in 2018
 dam_18_hourly <- aggregate(data_18$dam,
     by = list(hour = data_18$hour), FUN = mean)
@@ -114,8 +80,40 @@ dam_19_hourly <- aggregate(data_19$dam,
     by = list(hour = data_19$hour), FUN = mean)
 dam_19_hourly <- dam_19_hourly$x - mean(data_19$dam)
 
-# plot the hourly mean of dam in 2018 and 2019 in the same graph
+# plot the aggregated means
 x11()
+par(mfcol = c(2, 2))
+boxplot(data_18$dam, data_19$dam, names = c("2018", "2019"),
+    horizontal = TRUE,
+    main = "Boxplot of dam in 2018 and 2019",
+    xlab = "Year", ylab = "Dam", col = c("blue", "red")
+)
+legend("topleft", legend = c("2018", "2019"),
+    col = c("blue", "red"), lty = 1, cex = 0.8,
+    lwd = 5
+)
+abline(v = mean(data_18$dam), col = "black", lty = 2)
+abline(v = mean(data_19$dam), col = "black", lty = 2)
+plot(dam_18_monthly, type = "l",
+    col = "blue",
+    main = "Monthly mean of dam in 2018 and 2019",
+    xlab = "Month", ylab = "Dam",
+    lwd = 5)
+lines(dam_19_monthly, col = "red", lwd = 5)
+legend("topleft", legend = c("2018", "2019"),
+    col = c("blue", "red"), lty = 1, cex = 0.8,
+    lwd = 5
+)
+plot(dam_18_weekday, type = "l",
+    col = "blue",
+    main = "Dayly mean of dam in 2018 and 2019",
+    xlab = "Day", ylab = "Dam",
+    lwd = 5)
+lines(dam_19_weekday, col = "red", lwd = 5)
+legend("topleft", legend = c("2018", "2019"),
+    col = c("blue", "red"), lty = 1, cex = 0.8,
+    lwd = 5
+)
 plot(dam_18_hourly, type = "l",
     col = "blue",
     main = "Hourly mean of dam in 2018 and 2019",
