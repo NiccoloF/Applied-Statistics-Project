0bff63ca147b694d201d351adaeb42465f37a82a
rossanacometa <rossana.cometa@mail.polimi.it>
2023-04-18 14:58:09 +0200

    heatmap

--- Forecasting_Day_Ahead_Elettricity_prices.R
+++ Forecasting_Day_Ahead_Elettricity_prices.R
@@ -17,3 +17,43 @@ dim(test)
 str(test)
 summary(test)
 dimnames(test)
+
+quartz()
+boxplot(test)
+plot(test$dam,test$Swiss_import, col = 'blue')
+
+
+# heat map
+corr <- cor(Electricity_price)
+quartz()
+par(mar = c(100,4,4,2))
+heatmap(corr)
+
+# spiegazione nomi colonne
+# dam Day Ahead Market cioè il prezzo [...]
+# Swiss_import , export ecc
+# forecasted_load produzione prevista
+# hour, day month da togliere?? boh no son molto rilevanti
+# geothermal10: produzione nostra misurata alle 10am del giorno stesso
+# geothermal : geotermica (acqua termale, calore proveniente dal sottosuolo)
+# hydro: centrali idroelettriche
+# pv : fotovoltaico (non delle case)
+# self-consumption : quanto della propria produzione (non solo fotovoltaico) self consumato
+# thermal : carbone
+# wind : pale eoliche
+# peak : massimo (di cosa??)
+# valley : minimo idem
+# gas_price : prezzo gas
+qqnorm(test$dam)
+qqline(test$dam, col = 'red', lwd = '2')
+shapiro.test(test$dam)
+library(nortest)
+ad.test(test$dam) # non è gaussiana per niente
+col12 <- test[,c(1,2)]
+col13 <- test[,c(1,3)]
+par(mfrow=c(1,2))
+boxplot(col12)
+pairs(col12)
+
+plot(col12, col = 'gold')
+plot(col13)
